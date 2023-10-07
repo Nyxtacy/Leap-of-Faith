624e0a12ef99d2175c4ac9cf469b1f0dcbbd5b3e
Nyx <nyxter65@gmail.com>
2023-10-07 18:03:23 +0530

    Projects

---
+++ Projects/Fibohachiseries.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Fibohachiseries {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n,a=0,b=1,s;
+        System.out.println("Enter number of terms");
+        n = scanner.nextInt();
+        while(n>=0){
+            s=a+b;
+            System.out.println(a);
+            a=b;
+            b=s;
+            n=n-1;
+        }
+    }
+}
---
+++ Projects/Leapyear.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Leapyear {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("what is the year");
+        int year = scanner.nextInt();
+
+        if (year>=1582){
+            if ((year%4==0)&&(year%400==0)||(year%100!=0)){
+                System.out.println(year + " is a Leap year");
+            }else{
+                System.out.println(year + " is not a Leap year");
+            }
+        }else{
+            System.out.println("enter a year after 1582 as it is the first year in the georgian calendar");
+        }
+        
+        scanner.close();
+    }
+}
